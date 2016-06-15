c76d5995a2aca08af0bae63aeb00eed9948a7f03
Adam Beardsley <adam.p.beardsley@gmail.com>
2016-06-15 13:43:46 -0700

    fixing formatting of script

--- apb/HERA_IDR1_read_autos.py
+++ apb/HERA_IDR1_read_autos.py
@@ -13,11 +13,10 @@ yyglob = '/data6/HERA/data/2457458/*yy.uv'
 xxfilenames = glob(xxglob)
 yyfilenames = glob(yyglob)
 
-xxt,xxd,xxf = capo.arp.get_dict_of_uv_data(xxfilenames,polstr='xx',antstr='auto')
-nt,nchan = xxd[0,0]['xx'].shape
+xxt, xxd, xxf = capo.arp.get_dict_of_uv_data(xxfilenames, polstr='xx', antstr='auto')
+nt, nchan = xxd[0, 0]['xx'].shape
 # Do some coarse averaging
-xxd_ave = np.zeros((nant,nt,nchan/chanave),dtype=np.float64)
+xxd_ave = np.zeros((nant, nt, nchan / chanave), dtype=np.float64)
 for ant in xrange(128):
-    for chan in xrange(nchan/chanave):
-        xxd_ave[ant,:,chan] = np.mean(xxd[ant,ant]['xx'][:,(chan*chanave):((chan+1)*chanave)],axis=1)
-
+    for chan in xrange(nchan / chanave):
+        xxd_ave[ant, :, chan] = np.mean(xxd[ant, ant]['xx'][:, (chan * chanave):((chan + 1) * chanave)], axis=1)
