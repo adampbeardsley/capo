6c83f7fc553fc7a1d752f47d22ddf1b44c88c477
Adam Beardsley <adam.p.beardsley@gmail.com>
2016-06-17 11:35:55 -0700

    Clean up Tsky calc, using gaussian for paper beam now (still not great)

--- apb/Tsky_v_LST.py
+++ apb/Tsky_v_LST.py
@@ -9,15 +9,34 @@ from astropy.time import Time
 import matplotlib.pyplot as plt
 import healpy as hp
 
-hera_beam_file = '/home/beards/code/python/PRISim/prisim/data/beams/HERA_HFSS_X4Y2H_4900.hmap'
+hera_beam_file = '/data2/beards/instr_data/HERA_HFSS_X4Y2H_4900.hmap'
 
-df = 1.5625
+df = 1.5625  # 100 MHz / 64 averaged channels
 freqs = np.arange(100.0 + df / 2.0, 200.0, df)
 hours = np.arange(0.0, 24.0, .5)
 lsts = np.zeros_like(hours)
 nside = 32
 pols = ['X']  # Only have X beam right now
 HERA_Tsky = np.zeros((len(pols), freqs.shape[0], lsts.shape[0]))
+PAPER_Tsky = np.zeros((len(pols), freqs.shape[0], lsts.shape[0]))
+paper_width0 = np.pi / 2.0  # FWHM in radians
+
+# Read in HERA beam data, just use full sky for paper
+hera_beam = {}
+paper_beam = {}
+for poli, pol in enumerate(pols):
+    temp_im = fits.getdata(hera_beam_file, extname='BEAM_{0}'.format(pol))
+    temp_f = fits.getdata(hera_beam_file, extname='FREQS_{0}'.format(pol))
+    # Interpolate to the desired frequencies
+    func = interpolate.interp1d(temp_f, temp_im, kind='cubic', axis=1)
+    hera_beam[pol] = func(freqs)
+    paper_beam[pol] = np.zeros_like(hera_beam[pol])
+    paper_beam[pol][0, :] = 1
+    for fi, freq in enumerate(freqs):
+        paper_beam[pol][:, fi] = hp.smoothing(paper_beam[pol][:, fi],
+                                              fwhm=paper_width0 * freqs.mean() / freq,
+                                              verbose=False)
+
 # Set up the observer
 (latitude, longitude, elevation) = ('-30.7224', '21.4278', 1100)
 ov = GSMObserver()
@@ -25,23 +44,17 @@ ov.lon = longitude
 ov.lat = latitude
 ov.elev = elevation
 
-# Read in HERA beam data
-hera_beam = {}
-for poli, pol in enumerate(pols):
-    temp_im = fits.getdata(hera_beam_file, extname='BEAM_{0}'.format(pol))
-    temp_f = fits.getdata(hera_beam_file, extname='FREQS_{0}'.format(pol))
-    # Interpolate to the desired frequencies
-    func = interpolate.interp1d(temp_f, temp_im, kind='cubic', axis=1)
-    hera_beam[pol] = func(freqs)
-
 fig = plt.figure("Tsky calc")
 for poli, pol in enumerate(pols):
     for fi, freq in enumerate(freqs):
         print 'Forming HERA Tsky for frequency ' + str(freq) + ' MHz.'
-        # Rotate and project hera beam
-        beam = hp.orthview(hera_beam[pol][:, fi], rot=[0, 90], fig=fig.number,
-                           xsize=400, return_projected_map=True, half_sky=True)
-        beam[np.isinf(beam)] = np.nan
+        # Rotate and project hera beam (Need to figure out how to do this w/o making figure)
+        hbeam = hp.orthview(hera_beam[pol][:, fi], rot=[0, 90], fig=fig.number,
+                            xsize=400, return_projected_map=True, half_sky=True)
+        hbeam[np.isinf(hbeam)] = np.nan
+        pbeam = hp.orthview(paper_beam[pol][:, fi], rot=[0, 90], fig=fig.number,
+                            xsize=400, return_projected_map=True, half_sky=True)
+        pbeam[np.isinf(pbeam)] = np.nan
         for ti, t in enumerate(hours):
             plt.clf()
             dt = datetime(2013, 1, 1, np.int(t), np.int(60.0 * (t - np.floor(t))),
@@ -53,44 +66,15 @@ for poli, pol in enumerate(pols):
             sky = hp.orthview(d, fig=fig.number, xsize=400, return_projected_map=True,
                               half_sky=True)
             sky[np.isinf(sky)] = np.nan
-            HERA_Tsky[poli, fi, ti] = np.nanmean(beam * sky) / np.nanmean(beam)
+            HERA_Tsky[poli, fi, ti] = np.nanmean(hbeam * sky) / np.nanmean(hbeam)
+            PAPER_Tsky[poli, fi, ti] = np.nanmean(pbeam * sky) / np.nanmean(pbeam)
 
 inds = np.argsort(lsts)
 lsts = lsts[inds]
 HERA_Tsky = HERA_Tsky[:, :, inds]
+PAPER_Tsky = PAPER_Tsky[:, :, inds]
 
 Tsky_file = '/data2/beards/tmp/HERA_Tsky.npz'
 np.savez(Tsky_file, HERA_Tsky=HERA_Tsky, freqs=freqs, lsts=lsts)
-
-# Repeat for "PAPER beam" - basically all-sky for now
-PAPER_Tsky = np.zeros((len(pols), freqs.shape[0], lsts.shape[0]))
-paper_beam = {}
-for poli, pol in enumerate(pols):
-    paper_beam[pol] = np.ones_like(hera_beam[pol])
-
-# Rotate and project "paper beam" - do outside loop because I'm treating it as all-sky
-beam = hp.orthview(paper_beam[pol][:, fi], rot=[0, 90], fig=fig.number,
-                   xsize=400, return_projected_map=True, half_sky=True)
-beam[np.isinf(beam)] = np.nan
-for poli, pol in enumerate(pols):
-    for fi, freq in enumerate(freqs):
-        print 'Forming PAPER Tsky for frequency ' + str(freq) + ' MHz.'
-        for ti, t in enumerate(hours):
-            plt.clf()
-            dt = datetime(2013, 1, 1, np.int(t), np.int(60.0 * (t - np.floor(t))),
-                          np.int(60.0 * (60.0 * t - np.floor(t * 60.0))))
-            lsts[ti] = Time(dt).sidereal_time('apparent', longitude).hour
-            ov.date = dt
-            ov.generate(freq)
-            d = ov.view(fig=fig.number)
-            sky = hp.orthview(d, fig=fig.number, xsize=400, return_projected_map=True,
-                              half_sky=True)
-            sky[np.isinf(sky)] = np.nan
-            PAPER_Tsky[poli, fi, ti] = np.nanmean(beam * sky) / np.nanmean(beam)
-
-inds = np.argsort(lsts)
-lsts = lsts[inds]
-PAPER_Tsky = PAPER_Tsky[:, :, inds]
-
 Tsky_file = '/data2/beards/tmp/PAPER_Tsky.npz'
 np.savez(Tsky_file, PAPER_Tsky=PAPER_Tsky, freqs=freqs, lsts=lsts)
